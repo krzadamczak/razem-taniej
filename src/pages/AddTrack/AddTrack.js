1d431e50226d1b720ba723653482c2eaa5ac6ffe
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-09-05 21:34:44 +0200

    Prototyp strony

--- src/pages/AddTrack/AddTrack.js
+++ src/pages/AddTrack/AddTrack.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
+import Label from "../../components/Label/Label";
+import "./AddTrack.css";
 
 const AddTrack = () => {
     return (
         <>
-            <Input />
+            <div className='add-track'>
+                <div className='add-track__form'>
+                    <div className='add-track__inner'>
+                        <Label className='label' htmlFor='start-place'>
+                            Miejsce wyjazdu
+                        </Label>
+                        <Input className='input' type='text' id='start-place' />
+                    </div>
+
+                    <div className='add-track__inner'>
+                        <Label className='label' htmlFor='end-place'>
+                            Miejsce docelowe
+                        </Label>
+                        <Input className='input' type='text' id='end-place' />
+                    </div>
+                    <Button modifier='button--primary'>Dodaj przejazd</Button>
+                </div>
+            </div>
         </>
     );
 };
