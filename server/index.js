a8e34be221e4ce786f91907a41ddc77dc942bb21
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-09-07 21:29:32 +0200

    Integracja z mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+async function mongooseConnection() {
+    await mongoose.connect(
+        `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`,
+        () => {
+            console.log("Connected!");
+        }
+    );
+}
+
+mongooseConnection();
 
 app.use(bodyParser.json());
 
