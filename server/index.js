acb7e77b9240469e552f2b8ac96210a5ca1adcc8
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-09-08 22:52:06 +0200

    Drobne poprawki

--- server/index.js
+++ server/index.js
@@ -8,41 +8,24 @@ const Route = require("./Schemas/Route");
 dotenv.config();
 
 async function mongooseConnection() {
-    await mongoose.connect(
-        `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-        {
-            auth: {
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-            },
-            useNewUrlParser: true,
+    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+        auth: {
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
         },
-        () => {
-            console.log("Connected!");
-        }
-    );
+        useNewUrlParser: true,
+    });
 }
 
-// mongooseConnection();
-console.log(process.env);
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    auth: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-    },
-    useNewUrlParser: true,
-});
+mongooseConnection();
 
 app.use(bodyParser.json());
 
 app.post("/api/route", (req, res) => {
     const newRoute = async () => {
-        console.log(req.body);
         const { startingPlace, destination } = req.body;
-        console.log({ startingPlace, destination });
-        const route = new Route({ ...req.body });
+        const route = new Route({ startingPlace, destination });
         await route.save();
-        console.log(route);
     };
     newRoute();
     res.json({ response: "Request noticed!" });
