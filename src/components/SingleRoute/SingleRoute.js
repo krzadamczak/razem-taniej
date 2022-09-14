e3097c77268cb68aab6b016a481f890ac485d38b
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-09-14 21:00:00 +0200

    Renderowanie wpisów z bazy danych

---
+++ src/components/SingleRoute/SingleRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "../Button/Button";
+
+const SingleRoute = (props) => {
+    const { startingPlace, destination, departureTime, arrivalTime } = props;
+    return (
+        <div className='route'>
+            <div className='route__header'>
+                <div className='route__column'>
+                    <div className='route__place'>{startingPlace}</div>
+                    <div className='route__time'>{departureTime}</div>
+                </div>
+                <div className='arrow'>{">"}</div>
+                <div className='route__column'>
+                    <div className='route__place'>{destination}</div>
+                    <div className='route__time'>{arrivalTime}</div>
+                </div>
+            </div>
+            <div className='route__additional-informations'>
+                <p>Wolna liczba miejsc: 2</p>
+                <p>Bez zwierząt</p>
+            </div>
+            <div className='route__personal-info'>
+                <div className='route__driver'>
+                    <div className='route__name'>Krzysztof</div>
+                    <div className='route__rating'>5</div>
+                </div>
+                <Button variant='text'>Napisz do kierowcy</Button>
+            </div>
+        </div>
+    );
+};
+
+export default SingleRoute;
--- src/pages/AllRoutes/AllRoutes.js
+++ src/pages/AllRoutes/AllRoutes.js
@@ -1,35 +1,31 @@
 import React from "react";
-import Button from "../../components/Button/Button";
+import { useState, useEffect } from "react";
+import SingleRoute from "../../components/SingleRoute/SingleRoute";
 import "./AllRoutes.css";
 
 const AllRoutes = () => {
+    const [allRoutes, setAllRoutes] = useState([]);
+    useEffect(() => {
+        fetch("/api/routes")
+            .then((response) => response.json())
+            .then((response) => setAllRoutes([...response]));
+        return () => {};
+    }, []);
+
     return (
-        <div className='allRoutes'>
-            <div className='route'>
-                <div className='route__header'>
-                    <div className='route__column'>
-                        <div className='route__place'>Poznań</div>
-                        <div className='route__time'>8:00</div>
-                    </div>
-                    <div className='arrow'>{">"}</div>
-                    <div className='route__column'>
-                        <div className='route__place'>Poznań</div>
-                        <div className='route__time'>8:00</div>
-                    </div>
-                </div>
-                <div className='route__additional-informations'>
-                    <p>Wolna liczba miejsc: 2</p>
-                    <p>Bez zwierząt</p>
-                </div>
-                <div className='route__personal-info'>
-                    <div className='route__driver'>
-                        <div className='route__name'>Krzysztof</div>
-                        <div className='route__rating'>5</div>
-                    </div>
-                    <Button variant='text'>Napisz do kierowcy</Button>
-                </div>
-            </div>
-        </div>
+        <>
+            {allRoutes.map((item) => {
+                return (
+                    <SingleRoute
+                        key={item._id}
+                        startingPlace={item.startingPlace}
+                        destination={item.destination}
+                        arrivalTime={item.arrivalTime}
+                        startingTime={item.startingTime}
+                    />
+                );
+            })}
+        </>
     );
 };
 
