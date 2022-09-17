37a1be34c280ab95235a24b03e0caddb82840d57
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-09-17 16:30:06 +0200

    Hook do walidacji formularzy

---
+++ src/hooks/useForm.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export const useForm = (options) => {
+    const [data, setData] = useState(options?.initialValue || {});
+    const [errors, setErrors] = useState({});
+    const handleChange = (key, sanitizeFunction) => (e) => {
+        const value = sanitizeFunction ? sanitizeFunction(e.target.value) : e.target.value;
+        setData((prevData) => ({ ...prevData, [key]: value }));
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const validations = options?.validations;
+
+        if (validations) {
+            let valid = true;
+            const newErrors = {};
+            for (const key in validations) {
+                const value = data[key];
+                const validation = validations[key];
+
+                //Wyrażenie po prawej stronie - jeżeli value będzie np. pustrym stringiem zostanie przekonwerowane na true.
+                //Pusty string - false.
+                if (validation?.required?.value && !value) {
+                    valid = false;
+                    newErrors[key] = validation?.required?.message;
+                }
+
+                //Wyrażenie po prawej stronie - jeżeli wartość przejdzie test znaczy, że jest prawidłowa, dlatego musimy odwrócić warunek.
+                if (validation?.pattern?.value && !RegExp(validation.pattern.value).test(value)) {
+                    valid = false;
+                    newErrors[key] = validation.pattern.message;
+                }
+
+                //Wyrażenie po prawej stronie - musimy odwrócić warunek ponieważ jeżeli funkcja zwróci true znaczy, że wartość jest prawidłowa.
+                if (validation?.custom?.isValid && !validation.custom.isValid(value)) {
+                    valid = false;
+                    newErrors[key] = validation.custom.message;
+                }
+            }
+
+            if (!valid) {
+                setErrors(newErrors);
+                return;
+            }
+        }
+
+        setErrors({});
+
+        if (options?.onSubmit) {
+            options.onSubmit();
+        }
+    };
+
+    return {
+        data,
+        errors,
+        handleChange,
+        handleSubmit,
+    };
+};
