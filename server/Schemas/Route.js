6e91e6d54357699acfe9087f7c9070310daaadf3
Krzysztof Adamczak <krzadamczak@gmail.com>
2022-10-03 21:41:33 +0200

    Dodano nowe pole

--- server/Schemas/Route.js
+++ server/Schemas/Route.js
@@ -11,6 +11,7 @@ const routeSchema = new mongoose.Schema({
     createdAt: Date,
     modifiedAt: Date,
     createdBy: String,
+    emptySeats: Number,
 });
 
 module.exports = mongoose.model("Route", routeSchema);
